ad05e4881750b36c1994d7bd758c21a09a086dd8
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2023-10-16 16:32:30 +0300

    fixed hashPassword to accept parameter

--- src/cli/hashPassword.ts
+++ src/cli/hashPassword.ts
@@ -1,9 +1,22 @@
 import env from '../env';
 import * as bcrypt from 'bcrypt';
 
+const valueToHash = process.argv[2];
+if (!valueToHash) {
+  throw new Error(
+    'Pass password value to hash like: node hashPassword.js <passwordValue>',
+  );
+}
+
+const saltRounds = env.PASSWORD_SALT_ROUNDS;
+if (!saltRounds) {
+  throw new Error(
+    'Environment variable PASSWORD_SALT_ROUNDS must be a positive integer',
+  );
+}
+
 (async () => {
-  const saltRounds = env.PASSWORD_SALT_ROUNDS;
-  const passwordHash = await bcrypt.hash('password', saltRounds);
+  const passwordHash = await bcrypt.hash(valueToHash, saltRounds);
 
   console.log(passwordHash);
 })();
